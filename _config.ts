e3c0381cd6bf752530ba4c12f67e6853b96a8985
Sofía <sofimaturana830@gmail.com>
2025-11-10 08:56:04 -0300

    refactor: Ajustar dependencias y configuración

--- _config.ts
+++ _config.ts
@@ -1,13 +1,30 @@
 import lume from "lume/mod.ts";
 import unocss from "lume/plugins/unocss.ts";
 import { presetWind4 } from "unocss/preset-wind4";
+import { presetIcons } from "unocss/preset-icons/browser";
 
 const site = lume();
 
 site.use(unocss({
   reset: "tailwind",
   options: {
-    presets: [presetWind4],
+    presets: [() =>
+      presetIcons({
+        collections: {
+          // @ts-ignore Sí es una colección, cállate
+          noto: () =>
+            import("iconify/noto/icons.json", { with: { type: "json" } }).then((
+              i,
+            ) => i.default),
+        },
+      }), presetWind4],
+    theme: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        success: "var(--color-success)",
+      },
+    },
   },
 }));
 
